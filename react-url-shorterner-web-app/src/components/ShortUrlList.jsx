5db4db9f3d727dbdd95792799eb3cc598f9a6cd5
harshabambu <harshabambu@gmail.com>
2025-06-28 10:58:46 +0530

    setup is complted

---
+++ react-url-shorterner-web-app/src/components/ShortUrlList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShortUrlList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShortUrlList
---
+++ react-url-shorterner-web-app/src/components/ShortenerForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShortenerForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShortenerForm
